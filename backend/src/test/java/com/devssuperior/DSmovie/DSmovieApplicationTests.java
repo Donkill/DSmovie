6d81046e8aefe4814f255c18a4d6fe0a00c1e107
Danilo César <danilo.danilocesar@gmail.com>
2022-01-14 13:37:21 -0300

    Project created

---
+++ backend/src/test/java/com/devssuperior/DSmovie/DSmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devssuperior.DSmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DSmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
