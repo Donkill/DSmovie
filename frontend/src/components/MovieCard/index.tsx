9483fc2fe97683896fe8b0f31e72b048f861c31e
Danilo César <danilo.danilocesar@gmail.com>
2022-01-16 00:03:30 -0300

    useParams

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 import { Movie } from "types/movie";
 
 type Props = {
-    movie: Movie;
+    movie: Movie
 }
 
 function MovieCard( { movie } : Props)  {
 
-    return(
+    return (
 
         <div>
              <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
@@ -16,13 +16,13 @@ function MovieCard( { movie } : Props)  {
                    <h3>{movie.title}</h3>
                     <MovieScore />
         
-                    <Link to={`/form/${movie.id}1`}>
+                    <Link to={`form/${movie.id}`}>
                      <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                     </Link>
                         
              </div>
         </div>
-    )
+    );
 }
 
 export default MovieCard
